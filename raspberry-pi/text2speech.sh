836a37ebb398d9351f35a97f224d11c140d7e18f
Andy Laing <alaing@premierfarnell.com>
2014-05-18 16:08:33 +0000

    Speech to speech on Raspberry Pi

--- raspberry-pi/text2speech.sh
+++ raspberry-pi/text2speech.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-say() { local IFS=+;mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=$1&key=AIzaSyDE_hAqtnzpiRA5b3wQKoNwbM3gVYL_So8&q=${*:2}"; }
+say() { local IFS=+;curl --silent --user-agent "Mozilla/5.0" "http://translate.google.com/translate_tts?tl=$1&key=AIzaSyDE_hAqtnzpiRA5b3wQKoNwbM3gVYL_So8&q=${*:2}" > test.mp3;mplayer -ao alsa:device=hw=1,0 test.mp3; }
 say $1 ${*:2}
-#rm test.mp3  > /dev/null 2>&1
+rm test.mp3
+
