71c36bbc8ffd995f01b5095a833061343365e178
Christian Meilke <christian.meilke@googlemail.com>
2014-05-18 20:57:18 +0200

    Translation script

--- mac-os-x/gettext.py
+++ mac-os-x/gettext.py
@@ -8,6 +8,6 @@ input_string = ' '.join(sys.argv[1:])
 if input_string:
     d = json.loads(input_string)
     query = d['result'][0]['alternative'][0]['transcript']
-    print query
+    print(query)
 else:
-    print "Sorry, I am not sure."
+    print("Sorry, I am not sure.")
